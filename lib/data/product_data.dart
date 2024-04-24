7051609d7f74f32bc0efca4fcb4d3a7dcdc819f4
DIEGO_CORREA_MONTES <a.21308051280347@cbtis128.edu.mx>
2024-04-24 15:37:07 +0000

    Initial commit

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:correa0347/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/300zx.jpg",
+      title: "Nissan 300zx",
+      description: "Un clasico jdm de los 90s"),
+  ProductModel(
+      img: "assets/images/gtr.jpg",
+      title: "Nissan gtr r34",
+      description: "El famoso godzilla, una bestia"),
+  ProductModel(
+      img: "assets/images/mazda-rx-7.jpg",
+      title: "Mazda rx-7",
+      description: "La famosa leyenda con motor rotativo"),
+];
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:correa0347/screens/splash_screen.dart';
+
+void main(List<String> args) {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
